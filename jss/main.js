8401307cba69fa3509ec0cc672a22093db9ffc2e
booreda <ratr908@gmail.com>
2020-12-25 18:09:44 +0100

    js added

--- jss/main.js
+++ jss/main.js
@@ -1,29 +1,15 @@
-const todos=[
-    {
-        id:1,
-        text:"take ou the trash",
-        isCompleted:true,
-    },   {
-        id:2,
-        text:"meeting with boss",
-        isCompleted:true,
-    },{
-        id:3,
-        text:"dentist appt",
-        isCompleted:false,
-    },
-];
+ function Person(firstname, lastname, dob){
+     this.firstname=firstname;
+     this.lastname=lastname;
+     this.dob=new Date(dob);
+ }
+ const person1 = new Person ('john','doe','1-2-2000');
+ console.log(person1);
 
 
-console.log(todos[2].text);
-
-const todoJSON = JSON.stringify(todos);
-
-console.log(todoJSON);
 
 
-todos.forEach(todo => {
-    
-});
+
+
 
 
